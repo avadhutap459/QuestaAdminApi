40c0087356118e2cf85f83fefdf377d6bfca4dcc
AvadhutParab <avadhut.parab@sudlife.in>
2024-03-28 12:23:45 +0530

    Added Values controller

---
+++ QuestaAdminApi/Controllers/ValuesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuestaAdminApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Ok("Code Review");
+        }
+    }
+}
